a45d66271cc4f9eef592873dbe505fc11698f092
Drew Nelson <realseal177@gmail.com>
2022-02-17 15:35:50 -0800

    able to call individual recipes

--- api/model.js
+++ api/model.js
@@ -1,9 +1,11 @@
 const db = require('../data/db-config.js');
 
-// function getRecipeById(recipe_id) {
-//     return db('recipes as p')
-//         .join('steps as s', )
-// }
+async function getRecipeById(recipe_id) {
+    const recipe = await db('recipes as r')
+        .where('recipe_id', recipe_id)
+
+    return recipe;
+}
 
 module.exports = {
     getRecipeById,
