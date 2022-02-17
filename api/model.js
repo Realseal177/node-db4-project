b06fe609b82d4872acaacc7a54065bf5a372b9f4
Drew Nelson <realseal177@gmail.com>
2022-02-17 13:16:44 -0800

    knex tables done

---
+++ api/model.js
@@ -0,0 +1,10 @@
+const db = require('../data/db-config.js');
+
+// function getRecipeById(recipe_id) {
+//     return db('recipes as p')
+//         .join('steps as s', )
+// }
+
+module.exports = {
+    getRecipeById,
+}
